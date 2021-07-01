a5918a4fc1285e7ba16fcf62f4bbcafdf6f50b57
AroliSG <58828449+AroliSG@users.noreply.github.com>
2021-07-01 11:44:19 -0400

    last version of main.js
    now game is playeble, but this was done 1 months ago. do not really remember if theres some other bugs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 
 const rooms = require('./room.js');
+const map = new Map ();
 
 function onServerLoadScripts() {
 }
@@ -8,9 +9,9 @@ function onServerInitialise() {
     server.setServerName ( "Pinturillo - Draw and Play" );
     server.setGameModeText ( "JavaScript 0.1" );
     
-    server.setOption(VCMP.Server.Option.AutoJoinMessages, false);
+    server.setOption(15, false); // join messages
 }
-
+ 
 function onServerUnloadScripts() {
 
 }
@@ -29,269 +30,235 @@ function onPlayerSpawn(player) {
 function onPlayerConnect(player) {
     player.setName ("Player" + player.getId ());
     server.sendClientMessageToAll (`Player ${[player.getId ()]} joined the server.`);
-   
-    // adding player to tab list
-    server.getAllPlayers().forEach(p => {
-        let pList;
-        server.getAllPlayers().forEach(p => {
-            if (pList) pList +=  "," + p.getId ();
-            else pList = ""+p.getId ();
-        })
-        dataToClient (p, pList, 1);
-    })
 }
-
 
 function onPlayerDisconnect(player, reason) {
+    // deleting player from tab list
     server.getAllPlayers().forEach(p => {
-        // deleting player from tab list
         let pList;
         server.getAllPlayers().forEach(p => {
-            if (pList) pList +=  "," + p.getId ();
-            else pList = ""+p.getId ();
+            if (map.has (p.getId ()) && map.has (player.getId ()) && map.get (p.getId ()) == map.get (player.getId ()) && pList) pList +=  "," + p.getId ();
+            else if (map.has (p.getId ()) && map.has (player.getId ()) && map.get (p.getId ()) == map.get (player.getId ()) && pList == null) pList = p.getId ().toString ();
         });
-        dataToClient (p, pList, 1);
+        if (map.has (p.getId ()) && map.has (player.getId ()) && map.get (p.getId ()) == map.get (player.getId ())) {
+            dataToClient (p, pList, 1);
+
+            if (pList.split(',').length == 1) {
+                map.delete (map.get (player.getId ()) + "count"); 
+                if (map.has (map.get (player.getId ()))) map.delete (map.get (player.getId ())); // deleting saved draws
+            }
+            else map.set (map.get (player.getId ()) + "count", pList.split(',').length); // re-counting players once one left the game
+        }
     });
+    map.delete (player.getId ())
+    
+    // if player was a drawer, game will restart.
+    // let replyback = rooms.general.start (string, player);
+    // dataToClient (player, replyback, 3);
 }
-
-
+ 
+function onPlayerMessage(player, message) {
+    let Timer = rooms.general.Timer;
+    if (player.data.word != undefined && player.data.word.toLowerCase() == message.toLowerCase ()) {
+        console.log (Timer [map.get (player.getId ())] )
+    }
+}
+ 
 function onClientScriptData(player, stream) {
     let string = stream.readString(), integer = stream.readInt(); 
     switch (integer) {
         // drawing on guesser screen.
         case 1: {
             server.getAllPlayers().forEach(p => {
-                if (p.getId () != player.getId ()) dataToClient (p, string, 2);
+                if (p.getId () != player.getId ()) {
+                   if (map.has (p.getId ()) && map.get (p.getId ()) == string.split(',') [4]) dataToClient (p, string.substring(0, string.lastIndexOf(",")), 2);
+                }
             });
+
+            // detecting in which room the new player joined en,es,pt
+            if (map.has (string.split(',') [4]) && map.has (player.getId ()) && map.get (player.getId ()) == string.split(',') [4]) {
+
+                let target = map.get (string.split(',') [4]);
+                if (target && target.data.drawPos) target.data.drawPos += string.substring(0, string.lastIndexOf(",")) + "|";
+                else if (target) target.data.drawPos = string.substring(0, string.lastIndexOf(","));
+            }
         }
         break;
 
         // words to screen for the drawer
         case 2: {
-            let replyback = rooms.general.start (string);
+            let replyback = rooms.general.start (string, player, map);
             dataToClient (player, replyback, 3);
+
+            // drawing on new player screen
+            if (map.has (string)) { 
+                let target = map.get (string);
+                if (target.data.drawPos) {
+                    let draw = target.data.drawPos.split('|');
+                    draw.forEach (drawPos => { dataToClient (player, drawPos, 2); });
+                }
+            }
+
+            server.getAllPlayers().forEach(p => {
+                let pList;
+                server.getAllPlayers().forEach(p => {
+                    if (map.has (p.getId ()) && map.get (p.getId ()) == string && pList) pList +=  "," + p.getId ();
+                    else if (map.has (p.getId ()) && map.get (p.getId ()) == string && pList == null) pList = p.getId ().toString ();
+                });
+                if (map.has (p.getId ()) && map.get (p.getId ()) == string) {
+                    dataToClient (p, pList, 1);
+
+                    map.set (string + "count", pList.split(',').length); // counting players
+                }
+            });
         }
         break;
+
+        // activate timer, set word to guess
+        case 3: {
+            let Timer = rooms.general.Timer;
+            player.data.Timer = setInterval( () => {
+                // interval of time
+                server.getAllPlayers().forEach(p => {
+                    player.data.word = string;
+                    dataToClient (p, Timer [map.get (player.getId ())].toString (), 4);
+                });
+
+                // increasing time
+                Timer [map.get (player.getId ())] --;
+
+                //Time is Over
+                if (Timer [map.get (player.getId ())]== -1) {
+                    clearInterval(player.data.Timer);
+
+                    Timer [map.get (player.getId ())] = 100;
+
+                    // restarting game
+                    server.getAllPlayers().forEach(p => {
+                        if (map.get (map.get (p.getId ())).getId () != p.getId ()) {
+                            if (map.has (map.get (player.getId ()))) map.delete (map.get (player.getId ())); // deleting saved draws
+                            
+                        } 
+                    });
+                } 
+            }, 1000);
+
+
+        }
     }
 }
 
 
-function onPlayerEnterVehicle(player, vehicle, slot) {
+function restartgame () {
 
 }
 
+
+function onPlayerEnterVehicle(player, vehicle, slot) {
+}
 
 function onPlayerExitVehicle(player, vehicle) {
-
 }
-
 
 function onVehicleExplode(vehicle) {
-
 }
-
 
 function onPlayerCommand(player, message) {
-
-
 }
-
-
 
 function onPlayerCrashReport(player, crashLog) {
-
 }
-
 
 function onCheckPointExited(checkPoint, player) {
-
 }
-
 
 function onCheckPointEntered(checkPoint, player) {
-
 }
-
 
 function onPickupRespawn(pickup) {
-
 }
-
 
 function onPickupPicked(pickup, player) {
-
 }
-
 
 function onPickupPickAttempt(pickup, player) {
-
-
     return true;
 }
-
 
 function onObjectTouched(object, player) {
-
 }
-
 
 function onObjectShot(object, player, weaponId) {
-
 }
-
 
 function onVehicleRespawn(vehicle) {
-
 }
-
 
 function onVehicleUpdate(vehicle, updateType) {
-
 }
-
 
 function onPlayerSpectate(player, spectated) {
-
 }
-
 
 function onPlayerKeyBindUp(player, keyBindIndex) {
-
 }
-
 
 function onPlayerKeyBindDown(player, keyBindIndex) {
-
 }
-
 
 function onPlayerPrivateMessage(player, recipient, message) {
-
     return true;
 }
-
-
-function onPlayerMessage(player, message) {
-
-}
-
 
 function onPlayerAwayChange(player, isAway) {
-
 }
-
 
 function onPlayerEndTyping(player) {
-
-
 }
-
 
 function onPlayerBeginTyping(player) {
-
-
 }
-
 
 function onPlayerGameKeysChange(player, oldKeys, newKeys) {
-
 }
-
 
 function onPlayerCrouchChange(player, isCrouching) {
-
 }
-
 
 function onPlayerOnFireChange(player, isOnFire) {
-
 }
-
 
 function onPlayerActionChange(player, oldAction, newAction) {
-
 }
-
 
 function onPlayerStateChange(player, oldState, newState) {
-
 }
-
 
 function onPlayerNameChange(player, oldName, newName) {
-
 }
-
 
 function onPlayerRequestEnterVehicle(player, vehicle, slot) {
-
     return true;
-
 }
-
 
 function onPlayerDeath(player, killer, reason, bodyPart) {
-
 }
 
-
 function onPlayerRequestSpawn(player) {
-
-
     return true;
 }
 
-
 function onPlayerRequestClass(player, classIndex) {
-
 }
 
 function onPlayerModuleList(player, list) {
-
 }
-
-
-
-
-
-
 
 function onIncomingConnection(name, password, ip) {
     return name;
 }
 
-
-
-
-/*
- the following 5 events are very CPU and memory intensive, if you're not using them,
- comment them out or just remove them and the server will not process them,
- saving you alot of resources CPU/memory-wise and overall improving server performance
- */
-/*
- function onPlayerUpdate(player, updateType) {
- 
- }
- 
- function onPlayerMove(player, oldX, oldY, oldZ, newX, newY, newZ) {
- 
- }
- 
- 
- function onPlayerHealthChange(player, lastHP, newHP) {
- 
- }
- 
- function onPlayerArmourChange(player, lastArmour, newArmour) {
- 
- }
- 
- function onPlayerWeaponChange(player, oldWep, newWep) {
- 
- }
- */
-
-function dataToClient (p, ...args )
+function dataToClient (p, ...args)
 {
     const stream = new VCMPStream();   
     args.forEach(data => {
